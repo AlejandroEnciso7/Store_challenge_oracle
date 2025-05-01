e935b34129d370294022b1d6a0c5da3894e31ca9
AlejandroEnciso7 <133036654+AlejandroEnciso7@users.noreply.github.com>
2025-05-01 15:53:33 -0700

    added functions to visualize the data results from the analysis

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Store_challenge_oracle
  Data science analysis for Oracle store challenge
+
+Este repositorio se centra en realizar el analisis de datos de las ventas de 4 tiendas, con los resultados obtenidos por el analisís se ralizará la venta de la tienda con menor desempeño
+
+para poder correr el codigo y visualizar los resultados es importante importar las librerias de pandas, numpy y matplotlib, esto se puede realizar copiando y pegando lo siguiente:
+
+import pandas as pd
+import numpy as np
+import  matplotlib.pyplot as plt
+
+al correr el codigo se podran evidenciar en formato de string/texto los resultados y asi mismo al final de presentaran los graficos para poder visualizar mejor los datos
+
+# Analisís
+Tras realizar el analisís pertinente, se ha decidido que la tienda que debe ser vendida es la tienda número 4.
+
+Esto se debe a que la tienda número 4 es la que presenta menores ingresos entre todas las tiendas, según lo encontrado se debe a que la tienda no presenta una considerable menor cantidad de ventas de articulos, pero si una diferencia entre las categorias que se venden en la tienda 4 en particular.
+
+Además, la tienda 4 es la segunda tienda con peor calificación por parte de los clientes, solo superado por la tienda 1, sin embargo cabe recalcar que la tienda 1 es la que mayor número de ingresos genera.
+
+Adicionalmente, es importante recalcar que valdría la pena estudiar el servicio al cliente que ofrece la tienda número 3 pues cuenta con excelentes calificaciones y si se implementan sus estrategias en las otras tienda podría mejorarse la calificación en general de todas las tiendas.
+
+Finalmente, también se estudio el coste de envío promedio por tienda y se encontro que no hay una diferencia considerable que pueda influenciar la decisión sobre que tienda vender. Pues la diferencia entra la tienda con mayor y menor costo de envío es de tan solo 3 mil pesos COP (0.71 USD).
+
+Por lo tanto, la tienda menos rentable para el señor juan es la tienda número 4.
--- _main.py
+++ _main.py
@@ -7,6 +7,194 @@ tienda_2 = pd.read_csv('./data/tienda_2.csv')
 tienda_3 = pd.read_csv('./data/tienda_3.csv')
 tienda_4 = pd.read_csv('./data/tienda_4.csv')
 
+tienda_sample = tienda_1.head()
+print(tienda_sample)
 
-categorias_tienda_1 = tienda_1.groupby('Categoría del Producto')['Precio'].sum()
-print(categorias_tienda_1)
+ingreso_total_tienda_1 = tienda_1['Precio'].sum()
+print(f'Ingreso total tienda 1: {ingreso_total_tienda_1}')
+
+ingreso_total_tienda_2 = tienda_2['Precio'].sum()
+print(f'Ingreso total tienda 2: {ingreso_total_tienda_2}')
+
+ingreso_total_tienda_3 = tienda_3['Precio'].sum()
+print(f'Ingreso total tienda 3: {ingreso_total_tienda_3}')
+
+ingreso_total_tienda_4 = tienda_4['Precio'].sum()
+print(f'Ingreso total tienda 4: {ingreso_total_tienda_4}')
+
+categorias_tienda_1 = tienda_1.groupby('Categoría del Producto').count()
+print(categorias_tienda_1['Producto'])
+
+categorias_tienda_2 = tienda_2.groupby('Categoría del Producto').count()
+print(categorias_tienda_2['Producto'])
+
+categorias_tienda_3 = tienda_3.groupby('Categoría del Producto').count()
+print(categorias_tienda_3['Producto'])
+
+categorias_tienda_4 = tienda_4.groupby('Categoría del Producto').count()
+print(categorias_tienda_4['Producto'])
+
+calificacion_tienda_1= tienda_1['Calificación'].mean()
+print(f'Calificación promedio tienda 1: {calificacion_tienda_1}')
+
+calificacion_tienda_2 = tienda_2['Calificación'].mean()
+print(f'Calificación promedio tienda 2: {calificacion_tienda_2}')
+
+calificacion_tienda_3 = tienda_3['Calificación'].mean()
+print(f'Calificación promedio tienda 3: {calificacion_tienda_3}')
+
+calificacion_tienda_4 = tienda_4['Calificación'].mean()
+print(f'Calificación promedio tienda 4: {calificacion_tienda_4}')
+
+ventas_tienda_1 = tienda_1['Producto'].value_counts()
+print(ventas_tienda_1)
+
+producto_mas_vendido_tienda_1 = ventas_tienda_1.idxmax()
+cantidad_mas_vendida_tienda_1 = ventas_tienda_1.max()
+print(f'Producto más vendido tienda 1: {producto_mas_vendido_tienda_1}, con un total de {cantidad_mas_vendida_tienda_1} ventas.')
+
+producto_menos_vendido_tienda_1 = ventas_tienda_1.idxmin()
+cantidad_menos_vendida_tienda_1 = ventas_tienda_1.min()
+print(f'Producto menos vendido tienda 1: {producto_menos_vendido_tienda_1}, con un total de {cantidad_menos_vendida_tienda_1} ventas.') # como hacer que me muestre los empates?
+
+ventas_tienda_2 = tienda_2['Producto'].value_counts()
+print(ventas_tienda_2)
+
+producto_mas_vendido_tienda_2 = ventas_tienda_2.idxmax()
+cantidad_mas_vendida_tienda_2 = ventas_tienda_2.max()
+print(f'Producto más vendido tienda 2: {producto_mas_vendido_tienda_2}, con un total de {cantidad_mas_vendida_tienda_2} ventas.')
+
+producto_menos_vendido_tienda_2 = ventas_tienda_2.idxmin()
+cantidad_menos_vendida_tienda_2 = ventas_tienda_2.min()
+print(f'Producto menos vendido tienda 2: {producto_menos_vendido_tienda_2}, con un total de {cantidad_menos_vendida_tienda_2} ventas.') 
+
+ventas_tienda_3 = tienda_3['Producto'].value_counts()
+print(ventas_tienda_3)
+
+producto_mas_vendido_tienda_3 = ventas_tienda_3.idxmax()
+cantidad_mas_vendida_tienda_3 = ventas_tienda_3.max()
+print(f'Producto más vendido tienda 3: {producto_mas_vendido_tienda_3}, con un total de {cantidad_mas_vendida_tienda_3} ventas.')
+
+producto_menos_vendido_tienda_3 = ventas_tienda_3.idxmin()
+cantidad_menos_vendida_tienda_3 = ventas_tienda_3.min()
+print(f'Producto menos vendido tienda 3: {producto_menos_vendido_tienda_3}, con un total de {cantidad_menos_vendida_tienda_3} ventas.') 
+
+ventas_tienda_4 = tienda_4['Producto'].value_counts()
+print(ventas_tienda_4)
+
+producto_mas_vendido_tienda_4 = ventas_tienda_4.idxmax()
+cantidad_mas_vendida_tienda_4 = ventas_tienda_4.max()
+print(f'Producto más vendido tienda 4: {producto_mas_vendido_tienda_4}, con un total de {cantidad_mas_vendida_tienda_4} ventas.')
+
+producto_menos_vendido_tienda_4 = ventas_tienda_4.idxmin()
+cantidad_menos_vendida_tienda_4 = ventas_tienda_4.min()
+print(f'Producto menos vendido tienda 4: {producto_menos_vendido_tienda_4}, con un total de {cantidad_menos_vendida_tienda_4} ventas.') 
+
+promedio_envios_tienda_1= tienda_1['Costo de envío'].mean()
+print(f'el promedio del costo de envios de la tienda 1 es: {promedio_envios_tienda_1}')
+
+promedio_envios_tienda_2= tienda_2['Costo de envío'].mean()
+print(f'el promedio del costo de envios de la tienda 2 es: {promedio_envios_tienda_2}')
+
+promedio_envios_tienda_3= tienda_3['Costo de envío'].mean()
+print(f'el promedio del costo de envios de la tienda 3 es: {promedio_envios_tienda_3}')
+
+promedio_envios_tienda_4= tienda_4['Costo de envío'].mean()
+print(f'el promedio del costo de envios de la tienda 4 es: {promedio_envios_tienda_4}')
+
+#GRAFICO DE LINEAS DE INGRESO TOTAL POR TIENDA
+ingreso_tiendas = {'X': ['tienda_1', 'tienda_2', 'tienda_3', 'tienda_4'], 'Y': [ingreso_total_tienda_1, ingreso_total_tienda_2, ingreso_total_tienda_3, ingreso_total_tienda_4]}
+plt.plot(ingreso_tiendas['X'], ingreso_tiendas['Y'], marker='o', linestyle='-')
+# Personalizar el gráfico
+plt.xlabel('tienda')
+plt.ylabel('total ventas')
+plt.title('total de ingresos por cada tienda')
+plt.grid()
+plt.show() # Mostrar el gráfico
+
+
+# GRAFICOS DE BARRAS DE CANTIDAD DE PRODUCTOS POR CATEGORIA
+plt.figure(figsize=(10, 6))
+categorias_tienda_1['Producto'].plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Cantidad de productos por categoría tienda 1')
+plt.xlabel('Categoría del Producto')
+plt.ylabel('Cantidad')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+categorias_tienda_2['Producto'].plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Cantidad de productos por categoría tienda 2')
+plt.xlabel('Categoría del Producto')
+plt.ylabel('Cantidad')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+categorias_tienda_3['Producto'].plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Cantidad de productos por categoría tienda 3')
+plt.xlabel('Categoría del Producto')
+plt.ylabel('Cantidad')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+categorias_tienda_4['Producto'].plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Cantidad de productos por categoría tienda 4')
+plt.xlabel('Categoría del Producto')
+plt.ylabel('Cantidad')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+# GRAFICO DE LINEAS DE CALIFICACION PROMEDIO POR TIENDA
+
+ingreso_tiendas = {'X': ['tienda_1', 'tienda_2', 'tienda_3', 'tienda_4'], 'Y': [calificacion_tienda_1, calificacion_tienda_2, calificacion_tienda_3, calificacion_tienda_4]}
+plt.plot(ingreso_tiendas['X'], ingreso_tiendas['Y'], marker='o', linestyle='-')
+# Personalizar el gráfico
+plt.xlabel('tienda')
+plt.ylabel('calificacion promedio')
+plt.title('calificacion promedio de cada tienda')
+plt.grid()
+plt.show()
+
+# GRAFICOS DE BARRAS DE CANTIDAD DE VENTAS POR PRODUCTO
+
+plt.figure(figsize=(10, 6))
+ventas_tienda_1.plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Cantidad de ventas por producto tienda 1')
+plt.xlabel('Producto')
+plt.ylabel('Cantidad')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+ventas_tienda_2.plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Cantidad de ventas por producto tienda 2')
+plt.xlabel('Producto')
+plt.ylabel('Cantidad')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+ventas_tienda_3.plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Cantidad de ventas por producto tienda 3')
+plt.xlabel('Producto')
+plt.ylabel('Cantidad')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+ventas_tienda_4.plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Cantidad de ventas por producto tienda 4')
+plt.xlabel('Producto')
+plt.ylabel('Cantidad')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
